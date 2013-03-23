3543d60e3d84f169e3847e9c3e0c24a7402160a5
ForNeVeR <neverthness@gmail.com>
2013-03-23 16:35:56 +0700

    Auto format conversion, error reporting.

--- Diff-Word.ps1
+++ Diff-Word.ps1
@@ -4,13 +4,20 @@ param(
 )
 
 # Constants
+$wdDoNotSaveChanges = 0
 $wdCompareTargetNew = 2
 
-$word = New-Object -ComObject Word.Application
-$word.Visible = $true
-$document = $word.Documents.Open($BaseFileName, $true, $true)
-$document.Compare($ChangedFileName, [ref]"Comparison", [ref]$wdCompareTargetNew, [ref]$true, [ref]$true)
+try {
+	$word = New-Object -ComObject Word.Application
+	$word.Visible = $true
+	$document = $word.Documents.Open($BaseFileName, $false, $true)
+	$document.Compare($ChangedFileName, [ref]"Comparison", [ref]$wdCompareTargetNew, [ref]$true, [ref]$true)
 
-$word.ActiveDocument.Saved = 1
-#$wdDoNotSaveChanges = 0
-#$document.Close(wdDoNotSaveChanges)
+	$word.ActiveDocument.Saved = 1
+
+	# Now close the document so only compare results window persists:
+	$document.Close([ref]$wdDoNotSaveChanges)
+} catch {
+	Add-Type -AssemblyName System.Windows.Forms
+	[System.Windows.Forms.MessageBox]::Show($_.Exception)
+}
