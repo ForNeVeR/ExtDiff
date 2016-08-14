43d3354dc072244d1f12134113b4ecbe42b693b8
Friedrich von Never <friedrich@fornever.me>
2016-08-14 10:46:49 +0700

    Comparison: remove file readonly attribute and don't open files as readonly
    Microsoft Word refuses to compare readonly file with anything for some
    reason.

--- Diff-Word.ps1
+++ Diff-Word.ps1
@@ -3,6 +3,15 @@ param(
     $ChangedFileName
 )
 
+$ErrorActionPreference = 'Stop'
+
+# Remove the readonly attribute because Word is unable to compare readonly
+# files:
+$baseFile = Get-ChildItem $BaseFileName
+if ($baseFile.IsReadOnly) {
+    $baseFile.IsReadOnly = $false
+}
+
 # Constants
 $wdDoNotSaveChanges = 0
 $wdCompareTargetNew = 2
@@ -10,7 +19,7 @@ $wdCompareTargetNew = 2
 try {
 	$word = New-Object -ComObject Word.Application
 	$word.Visible = $true
-	$document = $word.Documents.Open($BaseFileName, $false, $true)
+	$document = $word.Documents.Open($BaseFileName, $false, $false)
 	$document.Compare($ChangedFileName, [ref]"Comparison", [ref]$wdCompareTargetNew, [ref]$true, [ref]$true)
 
 	$word.ActiveDocument.Saved = 1
