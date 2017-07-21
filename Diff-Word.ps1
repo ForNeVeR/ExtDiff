2a90820ec261b038892e90d09f2f8f5596eb30af
Friedrich von Never <friedrich@fornever.me>
2017-07-21 22:41:54 +0700

    Diff-Word: resolve file paths on start
    This should fix the issues with relative and PowerShell-only file paths
    (e.g. ~/file.docx).

--- Diff-Word.ps1
+++ Diff-Word.ps1
@@ -1,9 +1,16 @@
 param(
-    $BaseFileName,
-    $ChangedFileName
+    [string] $BaseFileName,
+    [string] $ChangedFileName
 )
 
 $ErrorActionPreference = 'Stop'
+
+function resolve($relativePath) {
+    (Resolve-Path $relativePath).Path
+}
+
+$BaseFileName = resolve $BaseFileName
+$ChangedFileName = resolve $ChangedFileName
 
 # Remove the readonly attribute because Word is unable to compare readonly
 # files:
@@ -17,16 +24,16 @@ $wdDoNotSaveChanges = 0
 $wdCompareTargetNew = 2
 
 try {
-	$word = New-Object -ComObject Word.Application
-	$word.Visible = $true
-	$document = $word.Documents.Open($BaseFileName, $false, $false)
-	$document.Compare($ChangedFileName, [ref]"Comparison", [ref]$wdCompareTargetNew, [ref]$true, [ref]$true)
+    $word = New-Object -ComObject Word.Application
+    $word.Visible = $true
+    $document = $word.Documents.Open($BaseFileName, $false, $false)
+    $document.Compare($ChangedFileName, [ref]"Comparison", [ref]$wdCompareTargetNew, [ref]$true, [ref]$true)
 
-	$word.ActiveDocument.Saved = 1
+    $word.ActiveDocument.Saved = 1
 
-	# Now close the document so only compare results window persists:
-	$document.Close([ref]$wdDoNotSaveChanges)
+    # Now close the document so only compare results window persists:
+    $document.Close([ref]$wdDoNotSaveChanges)
 } catch {
-	Add-Type -AssemblyName System.Windows.Forms
-	[System.Windows.Forms.MessageBox]::Show($_.Exception)
+    Add-Type -AssemblyName System.Windows.Forms
+    [System.Windows.Forms.MessageBox]::Show($_.Exception)
 }
